7de0965a0a15fa9ac37b2dd1043bd2bc87d57659
Nicolas Gard <nicolasg699@gmail.com>
2017-05-07 16:58:39 -0500

    added sound manager

--- src/resource_manager.cpp
+++ src/resource_manager.cpp
@@ -12,3 +12,18 @@ bool ResourceManager::loadTexture(std::string key, std::string filename) {
     texture_map[key] = texture;
     return true;
 }
+
+bool ResourceManager::loadSound(std::string key, std::string filename) {
+    sf::SoundBuffer sound_buffer;
+
+    if (!sound_buffer.loadFromFile("res/" + filename + ".png")) {
+        std::cout << "Error loading file \"" << filename << "\"!\n";
+        return false;
+    }
+
+    sound_buffer_map[key] = sound_buffer;
+
+    sound_map[key] = sf::Sound(sound_buffer_map[key]);
+
+    return true;
+}
--- src/resource_manager.hpp
+++ src/resource_manager.hpp
@@ -4,12 +4,16 @@
 #include <map>
 #include <string>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 class ResourceManager {
 public:
     std::map<std::string, sf::Texture> texture_map;
+    std::map<std::string, sf::SoundBuffer> sound_buffer_map;
+    std::map<std::string, sf::Sound> sound_map;
 
     bool loadTexture(std::string key, std::string filename);
+    bool loadSound(std::string key, std::string filename);
 };
 
 #endif
